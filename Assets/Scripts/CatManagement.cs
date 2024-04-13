704e8eec85f0abac89e65072db9faf4b60e24201
karmeloova <olga.maruszynska@gmail.com>
2024-04-13 14:34:13 +0200

    Menu+End
    Add Menu and End Scene

---
+++ Assets/Scripts/CatManagement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatManagement : MonoBehaviour
+{
+    public GameObject cat;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) Debug.Log("mysz");
+    }
+}
